2fc3b0151775903819dd11819998968a9e74c285
vijay9861 <vvijay9861@gmail.com>
2024-11-12 20:16:49 +0530

    Add project files.

---
+++ Controllers/SongsController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NewAPI.Models;
+using System.Collections;
+using System.Reflection.Metadata.Ecma335;
+
+namespace NewAPI.Controllers
+{
+    //[Route("[controller]")]
+    [Route("College")]
+    public class SongsController : Controller
+    {
+
+        private readonly UsbmContext context;
+        public SongsController(UsbmContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet("Student")]
+        public IActionResult GetAll()
+        {
+            var Data = context.Students.ToList();
+            if (Data == null)
+            {
+                return NotFound("No Data");
+            }
+            return Ok(Data);
+        }
+        [HttpGet("StudentGetBy/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var Data = context.Students.FirstOrDefault(x => x.StdId == id);
+            if (Data == null)
+            {
+                return NotFound("No Data");
+            }
+            return Ok(Data);
+        }
+        [HttpPost("AddNewStudent")]
+        public IActionResult InsertStd([Bind("StdName","Age","City")] Student Obj)
+        {
+
+            if (Obj != null)
+            {
+                context.Students.Add(Obj);
+                context.SaveChanges();
+                return Ok(Obj);
+            }
+            return NotFound("No Data Added");
+        }
+        [HttpPut("UpdateStudent")]
+        public IActionResult Update([Bind("StdId","StdName", "Age", "City")] Student Obj)
+        {
+            var Data = context.Students.FirstOrDefault(x => x.StdId == Obj.StdId);
+            if (Data != null)
+            {
+
+                Data.StdName=Obj.StdName;
+                Data.Age=Obj.Age;
+                Data.City=Obj.City;
+                context.SaveChanges();
+            }
+            return Ok(Data);
+
+        }
+        [HttpDelete("DeleteStudent")]
+        public IActionResult Delete(int id)
+        {
+            var Data = context.Students.FirstOrDefault(e => e.StdId == id);
+            if (Data == null)
+            {
+                return BadRequest("No Data");
+            }
+            context.Students.Remove(Data);
+            context.SaveChanges();
+            return Ok(Data);
+        }
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+
+using Microsoft.EntityFrameworkCore;
+using NewAPI.Models;
+
+namespace NewAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<UsbmContext>
+            (options => options.UseSqlServer(builder.Configuration.GetConnectionString("Data Source=DESKTOP-AU94EJS;Initial Catalog=Usbm;Integrated Security=True;TrustServerCertificate=True;Trusted_Connection=True")));
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
